56bffeecf4d55880786df7b15b3c0e7ff1f1fa47
andriyschudlo <3965258@gmail.com>
2022-10-10 14:06:02 +0300

    first commit

---
+++ gulp/tasks/fonts.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import fonter from 'gulp-fonter-fix';
+import ttf2woff2 from 'gulp-ttf2woff2';
+
+export const otfToTtf = () => {
+ return app.gulp.src(`${app.path.srcFolder}/fonts/*.otf`, {})
+ .pipe(app.plugins.plumber(
+  app.plugins.notify.onError({
+   title: "FONTS",
+   message: "Error: <%= error.message %>"
+  })
+ ))
+ .pipe(fonter({
+  formats: ['ttf']
+ }))
+ //выгружаем в исходную папку
+ .pipe(app.gulp.dest(`${app.path.srcFolder}/fonts/`))
+}
+
+export const ttfToWoff = () => {
+ return app.gulp.src(`${app.path.srcFolder}/fonts/*.ttf,`, {})
+ .pipe(app.plugins.plumber(
+  app.plugins.notify.onError({
+   title: "FONTS",
+   message: "Error: <%= error.message %>"
+  })
+ ))
+ //Конвертируем в .woff
+ .pipe(fonter({
+  formats: ['woff']
+ }))
+ .pipe(app.gulp.dest(`${app.path.build.fonts}`))
+ .pipe(app.gulp.src(`${app.path.srcFolder}/fonts/*.ttf`))
+ .pipe(ttf2woff2())
+ .pipe(app.gulp.dest(`${app.path.build.fonts}`));
+}
